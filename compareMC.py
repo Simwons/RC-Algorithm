38b068e8d985726fb4ee206b5ab2ff47c1129740
Simwons <luke5570@naver.com>
2023-05-07 20:35:47 +0900

    230507

--- compareMC.py
+++ compareMC.py
@@ -1,48 +1,72 @@
 import os
+import random
+import numpy as np
 import csv
 
-def resize_map(map, new_n, new_m):
-    n, m = len(map), len(map[0])
-    #new_map에 추가할 값 위치 저장
-    x_val = [int(i*n/new_n) for i in range(new_n)]
-    y_val = [int(j*m/new_m) for j in range(new_m)]
-    
-    new_map = [[0] * new_m for _ in range(new_n)]
-    
+def resize_course(course, new_n, new_m):
+    #코스 데이터 크기를 맵 데이터와 동일하게 변경
+    n, m = len(course), len(course[0])
+    x_val = np.linspace(0, n-1, new_n, dtype=int)
+    y_val = np.linspace(0, m-1, new_m, dtype=int)
+
+    new_course = np.zeros((new_n, new_m))
     for i in range(new_n):
         for j in range(new_m):
-            new_map[i][j] = map[x_val[i]][y_val[j]]
-    
-    return new_map
+            new_course[i][j] = course[x_val[i]][y_val[j]]
+            
+    for row in range(len(new_course)):
+        for col in range(len(new_course[0])):
+            value = new_course[row][col]
+            if value != 0:
+                for i in range(row, len(new_course)):
+                    for j in range(col, len(new_course[0])):
+                        if new_course[i][j] == value and (i != row or j != col):
+                            new_course[i][j] = 0
 
-def check_course(map, course):
-    #코스 데이터의 첫번째 위치 설정
-    current_pos = tuple(course[0][:2])
+    return new_course
+
+def check_course(map_data, course_data):
+    #각 코너의 위치를 positions에 저장
+    positions = []
+    for i in range(1, 101):
+        found = False
+        for y, row in enumerate(course_data):
+            for x, value in enumerate(row):
+                if value == i:
+                    positions.append((x+1, y+1))
+                    found = True
+                    break
+            if found:
+                break
+        if not found:
+            break
     
-    #순서대로 장애물이 있는지 확인
-    for next_pos in course[1:]:
-        #현재 위치와 다음 위치 사이 확인
-        if check_walls(map, current_pos, tuple(next_pos[:2])):
-            return False #장애물이 있으면 False
-        #현재 위치를 갱신
-        current_pos = tuple(next_pos[:2])
-        
-    return True #전체확인후 장애물이 없다면 True
+    #각 코너 사이에 장애물이 있는지 확인    
+    for i in range(1, len(positions)-1):
+        current_pos = positions[i]
+        next_pos = positions[i+1]
+        if check_walls(map_data, current_pos, next_pos):
+            return False #장애물이 있다면 False 반환
+    return True #장애물이 없다면 True 반환
+
+
 
 def check_walls(map, current_pos, next_pos):
-    # 현재 위치와 다음 위치의 좌표
+    #현재 위치와 다음 위치의 좌표
     x1, y1 = current_pos
     x2, y2 = next_pos
+    
     
     #각 좌표간의 차이
     dx = abs(x2 - x1)
     dy = abs(y2 - y1)
-    
-    # 이동방향 결정
+        
+    #이동방향 결정
     if x1 < x2:
         x_step = 1  #x축 상에서 +1
     else:
         x_step = -1 #x축 상에서 -1
+        
     if y1 < y2:
         y_step = 1  #y축 상에서 +1
     else:
@@ -50,44 +74,45 @@ def check_walls(map, current_pos, next_pos):
         
     #브레젠험 알고리즘 사용해 코스확인
     courseway = dx - dy
-    while x1 != x2 or y1 != y2:
-        if map[y1][x1] == 1:
-            return True #장애물이 있다면 while문 종료
+    
+    while x1 != x2 or y1 != y2:        
         courseway2 = courseway * 2
-        if courseway2 > -dy:
+        if courseway2 > dy:
             courseway -= dy
             x1 += x_step
         if courseway2 < dx:
             courseway += dx
             y1 += y_step
+            
+        if map[y1][x1] == 1:
+            return True #장애물이 있다면 while문 종료
+            
     return False #장애물이 없다면 반복
 
-def assign_course_to_map(map, course_list):
-    matched_course_data = None  #적합한 코스가 없을 경우 반환할 값
-    
-    for i, (file_name, course_data) in enumerate(course_list, start=1):
-        if check_course(map, course_data):
-            for j, pos in enumerate(course_data):
-                x, y = pos[:2]
-                map[int(y)][int(x)] = j + i * 10
-            matched_course_data = (file_name, course_data)  #적합한 코스가 발견되면 저장
-            break # 첫 번째로 매칭된 코스 데이터 반환 후 종료
-            
-    return matched_course_data  #모든 코스 확인후 반환
+def assign_course(map_data, course_data_list):
+    # 적합한 코스 저장할 리스트 생성
+    matched_courses = []
+
+    # 코스 데이터와 맵 데이터 비교
+    for course_data in course_data_list:
+        course = course_data[1]
+        is_matched = check_course(map_data, course)
+        if is_matched:
+            matched_courses.append(course_data)
+
+    # 적합한 코스 중 랜덤으로 선택
+    if matched_courses:
+        matched_course_data = random.choice(matched_courses)
+        return matched_course_data
+    else:
+        return None
+
 
 
 def main():
     #맵데이터 가져오기
-    map_data = []
-    with open('C:\RaceProject\RC-Algorithm\MapData\map_data.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            map_data.append([int(cell) for cell in row])
-    
-    new_map_data = resize_map(map_data, 10, 10)
-    print("10x10으로 변경된 맵데이터 :")
-    for row in new_map_data:
-        print(row)
+    map_data = np.loadtxt('C:\RaceProject\RC-Algorithm\MapData\map_data.csv', delimiter=',')
+    n, m = map_data.shape
         
     #코스데이터 가져오기
     course_data_list = []
@@ -96,12 +121,14 @@ def main():
             with open(os.path.join('C:\RaceProject\RC-Algorithm\CourseData', file_name), 'r') as csvfile:
                 course_data = list(csv.reader(csvfile))
                 course_data = list(map(tuple, [map(int, row) for row in course_data]))
+                course_data = resize_course(course_data, n, m)
                 course_data_list.append((file_name, course_data))
                 
-    matched_course_data = assign_course_to_map(new_map_data, course_data_list)
+    matched_course_data = assign_course(map_data, course_data_list)
 
     if matched_course_data:
-        print("다음 코스가 적합합니다:", matched_course_data[0])
+        print("다음 코스가 적합합니다:")
+        print(matched_course_data)
     else:
         print("적합한 코스가 없습니다")
 
