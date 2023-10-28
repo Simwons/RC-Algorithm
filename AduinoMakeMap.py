58716932d687f08110aca3e77178a0fa043a79b3
Simwons <luke5570@naver.com>
2023-10-28 11:46:56 +0900

    231028am

--- AduinoMakeMap.py
+++ AduinoMakeMap.py
@@ -4,12 +4,17 @@ import serial
 map_width = 100
 map_height = 100
 
-dft_front = 180
+onMaking = 1
 
 PORT = 'COM4'
 BaudRate = 9600
 
 ser = serial.Serial(PORT, BaudRate)
+
+#동작 시작 명령
+ser.write(b'StartSequence')
+response = ser.readline().decode().rstrip()
+print(response)
 
 #데이터 1만 존재하는 기본맵 생성
 map_data = [['1'] * map_width for _ in range(map_height)]
@@ -17,13 +22,6 @@ map_data = [['1'] * map_width for _ in range(map_height)]
 #차량의 시작 위치 설정 (x, y)
 car_pos = [map_width // 2, map_height // 2]
 
-#장애물 인식
-def detect_wall():
-    if map_data[car_pos[1]][car_pos[0]] == '1':
-        return True
-    else:
-        return False
-    
 #벽 제거 - 행
 def remove_rows():
     global map_data
@@ -35,55 +33,46 @@ def remove_columns():
     transposed_map = list(map(list, zip(*map_data)))
     transposed_map = [col for col in transposed_map if '0' in col]
     map_data = list(map(list, zip(*transposed_map)))
-
-#차량 회전
-def turn_car():
-    global dft_front, ser
-    if ser.in_waiting > 0:
-        data = ser.readline().decode().rstrip()
-        
-        if data == '01': #우회전
-            if dft_front == 0:
-                dft_front = 360
-            dft_front = dft_front - 90
-        elif data == '02': #좌회전
-            if dft_front == 360:
-                dft_front = 0
-            dft_front = dft_front + 90
-        elif data == '00': #전진
-            dft_front = dft_front
-            
+           
 #차량 이동
-def move_car():
-    car_front = None
-    new_x = None
-    new_y = None
+def move_car(mapCount, turnDir, seqCount, onMaking, nowSeq):
+    new_x = car_pos[0]
+    new_y = car_pos[1]  
     
     map_data[car_pos[1]][car_pos[0]] = '0'
     
-    #차량 방향 변경
-    turn_car()
-    car_front = dft_front
+    if nowSeq == seqCount:
+        if turnDir == 0:
+            #수평으로 직진하여 맵그림
+            new_x = new_x - mapCount #이동 
+            map_data[new_y][new_x:car_pos[0] + 1] = ['0'] * (car_pos[0] - new_x + 1)  #수평맵 업데이트
+            new_y = new_y + 1
     
-    #차량 방향에 따른 이동
-    if car_front == 180:
-        new_x = car_pos[0] + 1
-    if car_front == 270:
-        new_y = car_pos[1] + 1
-    if car_front == 90:
-        new_y = car_pos[1] - 1
-    if car_front == 360 or 0:
-        new_x = car_pos[0] - 1
-        
+        else:
+            #수평으로 직진하여 맵 그림
+            new_x = new_x + mapCount  #이동
+            map_data[new_y][car_pos[0]:new_x + 1] = ['0'] * (new_x - car_pos[0] + 1)  #수평맵 업데이트
+            new_y = new_y - 1
+    
+    else:
+        new_y = new_y - mapCount #이동
+        map_data[new_y:car_pos[1]][new_x] = ['0'] * (car_pos[0] - new_y + 1) #수직맵 업데이트
+        nowSeq = seqCount
+    
     car_pos[0] = new_x
     car_pos[1] = new_y
-
+        
+    if nowSeq == 2:
+        onMaking = 0
+    
+    return onMaking, nowSeq
+        
 #맵데이터 제작
-for _ in range(10000):
-    move_car()
-
-    if detect_wall():
-        map_data[car_pos[1]][car_pos[0]] = '1'
+while (onMaking):
+    if ser.in_waiting:
+        data = ser.readline().decode().rstrip()
+        mapCount, turnDir, seqCount = map(int, data.split(","))
+        onMaking = move_car(mapCount, turnDir, seqCount, onMaking)
 
 #벽 제거
 remove_rows()
